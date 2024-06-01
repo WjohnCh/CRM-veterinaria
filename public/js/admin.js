bcbe682011e381f3fb4f25556203c443e1f1dfe3
WjohnCh22 <wjohnch22@gmail.com>
2024-06-01 09:45:57 -0500

    Se mejoró el aspecto de navbarAdmin

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,12 +1,34 @@
+
 document.addEventListener("DOMContentLoaded", function() {
         const contenedorCita = document.getElementById('contenedor-citas-option');
-        const opcionesCita = document.getElementById('opciones-cita')
+        const opcionesCita = document.querySelectorAll('.opciones-cita')
+        const opcionesAdmin = document.querySelectorAll('.option-admin');
+        const flechaGira = document.querySelectorAll('.flecha-admin-opcion')
+        // contenedorCita.addEventListener('click',()=>{
+        //         opcionesCita.classList.toggle("motrar-elemento");
+        //         flechaGira.classList.toggle('flecha-admin-opcion');
+        // })
 
-        contenedorCita.addEventListener('click',()=>{
-            if (opcionesCita.style.display === 'none' || opcionesCita.style.display === '') {
-                opcionesCita.style.display = 'block';
-            } else {
-                opcionesCita.style.display = 'none';
-            }
-        })
+
+// LOGICA PARA CAMBIAR DE COLOR UN BOTON
+        opcionesAdmin.forEach(function(element) {
+            element.addEventListener('click', function() {
+                let i =0;
+                let aux = this;
+                const flecha = this.querySelector('.flecha-admin');
+                // Eliminar la clase 'active' de todos los elementos
+                opcionesAdmin.forEach(function(el) {
+                    if(aux != el){
+                        opcionesCita[i].classList.remove("motrar-elemento");
+                        flechaGira[i++].classList.add("flecha-admin-opcion");
+                        el.classList.remove('active');
+                    }else{
+                        i++;
+                    }
+                    });
+                this.nextElementSibling.classList.toggle("motrar-elemento");
+                flecha.classList.toggle('flecha-admin-opcion');
+                this.classList.add('active'); // this hace referencia al elemento que activó el evento
+            });
+        });
 })
